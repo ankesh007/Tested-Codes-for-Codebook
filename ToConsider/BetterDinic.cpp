20e70695b5ae31d41b3316f84a9125911a1128e9
LogicalFreak <ronakgreatagarwal@gmail.com>
2018-12-16 01:21:09 +0530

    Add files via upload

---
+++ ToConsider/BetterDinic.cpp
@@ -0,0 +1,72 @@
+struct Edge {
+    int a, b, cap, flow;
+};
+struct MaxFlow {
+    int n, s, t;
+    vector<int> d, ptr, q;
+    vector< Edge > e;
+    vector< vector<int> > g;
+    int i,j;
+    MaxFlow(int n) : n(n), d(n), ptr(n), q(n), g(n) {
+        e.clear();
+        for(int i=0;i<n;i++) {
+            g[i].clear();
+            ptr[i] = 0;
+        }
+    }
+    void addEdge(int a, int b, int cap) {
+        Edge e1 = { a, b, cap, 0 };
+        Edge e2 = { b, a, 0, 0 };
+        g[a].push_back( (int) e.size() );
+        e.push_back(e1);
+        g[b].push_back( (int) e.size() );
+        e.push_back(e2);
+    }
+    int getMaxFlow(int _s, int _t) {
+        s = _s; t = _t;
+        int flow = 0;
+        for (;;) {
+            if (!bfs()) break;
+            for(int i=0;i<n;i++) ptr[i] = 0;
+            while (int pushed = dfs(s, INF))
+                flow += pushed;
+        }
+        return flow;
+    }
+private:
+    bool bfs() {
+        int qh = 0, qt = 0;
+        q[qt++] = s;
+        for(int i=0;i<n;i++) d[i] = -1;
+        d[s] = 0;
+
+        while (qh < qt && d[t] == -1) {
+            int v = q[qh++];
+            int gv_sz=g[v].size();
+            for(int i=0;i<gv_sz;i++) {
+                int id = g[v][i], to = e[id].b;
+                if (d[to] == -1 && e[id].flow < e[id].cap) {
+                    q[qt++] = to;
+                    d[to] = d[v] + 1;
+                }
+            }
+        }
+        return d[t] != -1;
+    }
+    int dfs (int v, int flow) {
+        if (!flow) return 0;
+        if (v == t) return flow;
+        for (; ptr[v] < (int)g[v].size(); ++ptr[v]) {
+            int id = g[v][ptr[v]],
+                to = e[id].b;
+            if (d[to] != d[v] + 1) continue;
+            int pushed = dfs(to, min(flow, e[id].cap - e[id].flow));
+            if (pushed) {
+                e[id].flow += pushed;
+                e[id^1].flow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+};
--- ToConsider/complexfft_module.cpp
+++ ToConsider/complexfft_module.cpp
@@ -1,17 +1,10 @@
 // Instructions for using this: Nothing it is very obvious to use
 
-struct Complex{
-	ld x,y;
-	Complex operator+(const Complex &b){ return {x+b.x,y+b.y}; }
-	Complex operator-(const Complex &b){ return {x-b.x,y-b.y}; }
-	Complex operator*(const Complex &b){ return { x*b.x - y*b.y , x*b.y + y*b.x }; }
-	Complex operator/(const ld &b){ return {x/b, y/b}; }
-	ld real(){ return x;}
-};
+typedef complex<long double> Cld;
 class FFT{
 public:
 	static const ld PI;
-	static void cfft (vector<Complex> &L,int invert) {
+	static void cfft (vector<Cld> &L,int invert) {
 		int n = (int) L.size();
 		for(int i=1,j=0 ; i<n ; i++){
 			int bit = n>>1 ;
@@ -22,25 +15,17 @@ public:
 		for(int len=2 ; len<=n ; len<<=1){
 			int l2 = (len/2);
 			ld theta = (PI/l2);
-			Complex wlen = { cos(theta),(invert ? -1 : 1)*sin(theta) }; 
+			Cld wlen = polar(1.0L,(invert ? -1 : 1)*theta);
 			for(int i=0 ; i<n ; i+=len){
-				Complex w = {1.0,0.0};
+				Cld w(1.0,0.0);
 				for(int j=0 ; j<l2 ; j++, w=(w*wlen)){
-					Complex u = L[i+j]; Complex v = w*L[i+j+l2];
+					Cld u = L[i+j]; Cld v = w*L[i+j+l2];
 					L[i+j] = (u+v); L[i+j+l2] = (u-v);
 				}
 			}
 		}
 		if(invert)
-			for(int i=0 ; i<n ; i++) L[i] = L[i]/n;
+			for(int i=0 ; i<n ; i++) L[i] = L[i]/((ld) n);
 	}
 };
 const ld FFT::PI = acos(-1.0);
-
-cA.resize(new_n,zero);
-fill(cA.begin(),cA.end(),zero);
-FFT::cfft(cA,0);
-for(int i=0;i<new_n;i++)
-	cA[i]=cA[i]*cB[i];
-for(int i=0;i<new_n;i++)	
-	cD[i]+=llround(cA[i].x);
